bd8aa62f12d315578b9b01cfb5861525d9a48d98
GitCARB <85479240+GitCARB@users.noreply.github.com>
2023-10-25 23:06:27 -0500

    Se configura el puerto y conexion a bd.

---
+++ src/database.js
@@ -0,0 +1,9 @@
+const { default: mongoose } = require('mongoose');
+//const mongoose = require('mongoose');
+const { database } = require('./keys');
+
+mongoose.connect(database.URI, {
+    useNewUrlParser: true
+})
+    .then(db => console.log('La base de datos esta conectada '))
+    .catch(err => console.error(err));
--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ const express =  require('express');
 
 const config = require('./server/config');
 
+// database
+require('./database');
+
+
 const app = config(express());
 
+// inicializando el servidor
 app.listen(app.get('port'),() => {
-    console.log('Server on port', app.get('port')); 
+    console.log('Servidor en el puerto', app.get('port')); 
 })
